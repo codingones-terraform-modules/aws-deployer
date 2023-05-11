9aa17ddb792344d758c788cebcab96545c2b8220
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-12 01:02:53 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Terraform AWS Service Infrastructure
+
+## About
+
+Deploy an AWS Iam user
+
+## Contribution
+
+This repository is not open to external contribution yet.
+I you'd like to please send me an email.
+
+## Built With
+
+### Languages & Frameworks
+
+- [Terraform](https://www.terraform.io/) is an open-source infrastructure-as-code tool that enables provisioning, management, and versioning of cloud, on-premises, and third-party resources using declarative configuration files.
+
+## License
+
+See the [LICENSE.md](./LICENSE.md) of the repository.
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_organizational_unit" {
+  description = "The organization name in aws organizations"
+  nullable    = false
+  default     = false
+}
+
+variable "service" {
+  description = "The deployer service name in the Project-Service-Layer architecture"
+  nullable    = false
+  default     = false
+}
+
+variable "service_policy" {
+  description = "The deployer group policy"
+  type        = string
+  nullable    = false
+  default     = false
+}
+
+variable "name" {
+  description = "The deployer user name"
+  type        = string
+  nullable    = false
+  default     = false
+}
